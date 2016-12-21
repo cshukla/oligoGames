a91bd63715082e5b2db4eb4cf2fe7bf3e21c92a8
kdkorthauer <kdkorthauer@gmail.com>
2016-12-21 16:08:10 -0500

    reorder fns, add log transform param

--- R/DRfinder.R
+++ R/DRfinder.R
@@ -1,3 +1,269 @@
+#' A function for the WGBS version of the 'BumphunterEngine' function with 'LM'
+#' and 'GLM'
+#'
+#' This function is the WGBS version of the 'BumphunterEngine' function with 'LM'
+#' and 'GLM'
+#'
+#' @param OligoSignal
+#' @param conditionLabels
+#' @param design
+#' @param coef
+#' @param minInSpan Defaults to 10
+#' @param minNum Defaults to 70
+#' @param minNumRegion Defaults to 5
+#' @param cutoff Defaults to NULL
+#' @param nullMethod Defaults to GLMM
+#' @param smooth Defaults to FALSE
+#' @param bpSpan Defaults to 1000
+#' @param verbose Defaults to TRUE
+#' @param workers Defaults to NULL
+#' @param logT
+#' @param sampleSize Defaults to (ncol(OligoSignal)-1)/2
+#' @param maxPerms 
+#' @import
+#' @keywords inference
+#' @return
+#' @export
+
+DRfinder <- function(OligoSignal, 
+                     conditionLabels=c("condition1", "condition2"),
+                     minInSpan=10,
+                     design,
+                     coef = 2, minNum=70, bpSpan=1000, 
+                     minNumRegion=5, 
+                     cutoff = NULL, 
+                     smooth = FALSE,
+                     verbose = TRUE,
+                     workers = NULL, 
+                     sampleSize=(ncol(OligoSignal)-1)/2,
+                     maxPerms=50, logT=TRUE){
+  
+  cond <- c(rep(conditionLabels[1], sampleSize), 
+            rep(conditionLabels[2], sampleSize))
+  design = model.matrix(~cond)
+  
+  nlocs = nrow(OligoSignal) 
+  oligo.mat = OligoSignal[,-1]
+  
+  chr = rownames(OligoSignal)
+  pos = OligoSignal[,1]
+  rm(OligoSignal)
+  
+  maxGap <- maxGapSmooth <-max(diff(pos))+1
+  
+  # get observed stats
+  
+  res = bumphunt(oligo.mat, 
+                 minInSpan=minInSpan,
+                 design = design, chr = chr, pos = pos, 
+                 coef = coef, minNum=minNum, maxGapSmooth=maxGapSmooth, 
+                 minNumRegion=minNumRegion,
+                 cutoff = cutoff, maxGap = maxGap, 
+                 smooth = smooth, 
+                 verbose = verbose,
+                 workers = workers, logT=logT) 
+  
+  
+  if (nrow(design)%%2==0){
+    perms <- combn(seq(1, nrow(design)), sampleSize)
+    perms <- perms[, 2:(ncol(perms)/2)]
+    res.flip <- NULL
+    
+    if (maxPerms < ncol(perms)){
+      # subset on 'balanced perms'
+      if (sampleSize > 3 & sampleSize < 6){ 
+        sg <- apply(perms, 2, function(x) sum(x > sampleSize))
+        perms <- perms[, sg < (sampleSize-1) & sg >= 2 ]
+        maxPerms <- min(maxPerms, ncol(perms))
+      }else if(sampleSize >= 6){
+        sg <- apply(perms, 2, function(x) sum(x > sampleSize))
+        perms <- perms[, sg >= floor(sampleSize/2) & sg <= ceiling(sampleSize/2) ]
+      }
+      perms <- perms[,sort(sample(1:ncol(perms), maxPerms, replace=FALSE))]
+    }
+    
+    # Now rerun on flipped designs and concatenate results
+    for (j in 1:ncol(perms)){
+      reorder <- perms[,j]
+      designr <- design
+      designr[,2] <- 0
+      designr[reorder,2] <- 1
+      
+      res.flip.p = bumphunt(oligo.mat, 
+                            minInSpan=minInSpan,
+                            design = designr, chr = chr, pos = pos, 
+                            coef = coef, minNum=minNum, 
+                            maxGapSmooth=maxGapSmooth, 
+                            minNumRegion=minNumRegion,
+                            cutoff = cutoff,  maxGap = maxGap, 
+                            smooth = smooth,
+                            verbose = verbose,
+                            workers = workers, logT=logT) 
+      res.flip.p$permNum <- j 
+      
+      res.flip <- rbind(res.flip, res.flip.p)
+      print(paste0(j, " out of ", ncol(perms), " permutations completed"))
+    }	
+  }else{
+    stop("Error: Currently only balanced designs supported")
+  }
+  rm(res.flip.p)
+  
+  perm.ordered <- c(sort(abs(res$stat), method="quick"), Inf)
+  pval <- rep(NA, nrow(res))
+  pval[!is.na(res$stat)] <- (1 + 
+                               sapply(abs(res$stat[!is.na(res$stat)]), 
+                                      function(x) length(perm.ordered) - 
+                                        min(which(x <= perm.ordered)))) /
+    (1 + sum(!is.na(res$stat)))							
+  res$pval <- pval
+  res$qval <- p.adjust(pval, method="BH")
+  return(res)
+}
+
+
+#' A function for the WGBS version of the 'BumphunterEngine' function with 'LM'
+#' and 'GLM'
+#'
+#' This function is the WGBS version of the 'BumphunterEngine' function with 'LM'
+#' and 'GLM'
+#'
+#' @param oligo.mat
+#' @param design
+#' @param chr Defaults to NULL
+#' @param pos
+#' @param cluster Defaults to NULL
+#' @param coef
+#' @param minInSpan Defaults to 10
+#' @param minNum Defaults to 10
+#' @param minNumRegion Defaults to 5
+#' @param cutoff Defaults to NULL
+#' @param maxGap Defaults to 50
+#' @param maxGapSmooth Defaults to 1e8
+#' @param smooth Defaults to FALSE
+#' @param bpSpan Defaults to 100
+#' @param verbose Defaults to TRUE
+#' @param workers Defaults to NULL
+#' @param logT
+#' @inheritParams DRfinder
+#' @import
+#' @keywords inference
+#' @return
+
+##The WGBS version of the 'BumphunterEngine' function with 'LM' and 'GLM'
+bumphunt = function(oligo.mat, design, 
+                    chr = NULL, pos, coef = 2, minInSpan=10, minNum=10, 
+                    minNumRegion=5,
+                    cutoff = NULL, maxGap = 50, maxGapSmooth=50,
+                    smooth = FALSE, bpSpan=100,
+                    verbose = TRUE, workers=NULL, logT=TRUE, ...)
+{
+  if (!is.matrix(oligo.mat))
+    stop("'oligo.mat' must be a matrices.")
+  if (ncol(oligo.mat) != nrow(design))
+    stop("Total number of columns in 'oligo.mat' must  match number of rows of 'design'")
+  if (!(is.null(cutoff) || length(cutoff) %in% 1:2))
+    stop("'cutoff' has to be either NULL or a vector of length 1 or 2")
+  if (length(cutoff) == 2)
+    cutoff <- sort(cutoff)
+  
+  if (!getDoParRegistered())
+    registerDoSEQ()
+  registerDoParallel()
+  if (is.null(workers)) { workers <- 1 }
+  backend <- getDoParName()
+  version <- getDoParVersion()
+  subverbose <- max(as.integer(verbose) - 1L, 0)
+  if (verbose) {
+    if (workers == 1) {
+      mes <- "Using a single core (backend: %s, version: %s)."
+      message(sprintf(mes, backend, version))
+    }else {
+      mes <- "Parallelizing using %s workers/cores (backend: %s, version: %s)."
+      message(sprintf(mes, workers, backend, version))
+    }
+  }
+  if (is.null(chr))
+    chr <- rep("Unspecified", length(pos))
+  if (is.factor(chr))
+    chr <- as.character(chr)
+  
+  cluster <- bumphunter:::clusterMaker(chr, pos, maxGap = maxGap)
+  
+  if (verbose)
+    message("Computing coefficients.")
+  
+  if(logT){
+    est <- bumphunter:::.getEstimate(log2(oligo.mat+1), 
+                                     design, coef, full=TRUE) 
+  }else{
+    est <- bumphunter:::.getEstimate(oligo.mat, 
+                                     design, coef, full=TRUE) 
+  }
+  rawBeta <- est$coef
+  
+  if (smooth) {
+    sd.raw = est$sigma*est$stdev.unscaled
+    weights <- sd.raw 
+    weights[sd.raw < 10^-5] = mean(sd.raw[sd.raw > 10^-5]) 
+    rm(sd.raw)
+  }
+  rm(est)
+  gc()
+  
+  if (smooth) {
+    if (verbose)
+      message("Smoothing coefficients.")
+    
+    beta <- vector("list", 3)
+    beta[[1]] <- beta[[2]] <- rep(NA, length(pos))
+    for (chromosome in unique(chr)){
+      beta.tmp <- smoother(y = rawBeta[chr==chromosome],
+                           x = pos[chr==chromosome],
+                           workers=workers, chr=chr[chr==chromosome],
+                           maxGapSmooth=maxGapSmooth, weights = weights,
+                           minNum = minNum, minInSpan = minInSpan, bpSpan = bpSpan,
+                           verbose = verbose)
+      beta[[1]][chr==chromosome] <- beta.tmp[[1]]
+      beta[[2]][chr==chromosome] <- beta.tmp[[2]]
+    }
+    beta[[3]] <- beta.tmp[[3]]
+    names(beta) <- names(beta.tmp)
+    rm(beta.tmp)
+    Index <- which(beta$smoothed)
+    beta <- beta$fitted
+    
+  }else {
+    beta <- rawBeta
+    Index <- seq(along = beta)
+  }
+  rm(rawBeta)
+  gc()
+  if (verbose)
+    message("Finding regions.")
+  tab <- regionFinder(x = beta, chr = chr, pos = pos, cluster = cluster,
+                      cutoff = cutoff, ind = Index, minNumRegion = minNumRegion,
+                      oligo.mat = oligo.mat,
+                      verbose = TRUE,
+                      design = design, workers=workers, logT=logT)
+  rm(beta);
+  rm(chr);
+  rm(pos);
+  gc()
+  if (nrow(tab) == 0) {
+    if (verbose)
+      message("No candidate regions found!")
+    return(NA)
+  }else {
+    if (verbose)
+      message(sprintf("Found %s candidate regions.", nrow(tab)))
+  }
+  
+  return(table = tab)
+}
+
+
+
 #' A function to
 #'
 #' This function performs
@@ -27,7 +293,7 @@ regionFinder <- function(x, chr, pos, cluster=NULL, y=x,
                          maxGap, cutoff=quantile(abs(x), 0.99),
                          assumeSorted = FALSE, oligo.mat=oligo.mat,
                          verbose = TRUE,
-                         design=design, workers=workers){
+                         design=design, workers=workers, logT=TRUE){
   if(any(is.na(x[ind]))){
     warning("NAs found and removed. ind changed.")
     ind <- intersect(which(!is.na(x)),ind)
@@ -54,7 +320,11 @@ regionFinder <- function(x, chr, pos, cluster=NULL, y=x,
   stat.OligoSignal.spline <- function(ix){
     X = as.vector(sapply(design[,2], function(x) rep(x, nrow(oligo.mat[ix,]))))
     L = as.vector(rep(pos[ix], nrow(design)))
-    Y = log2(oligo.mat[ix,] + 1)
+    if(logT){
+      Y = log2(oligo.mat[ix,] + 1)
+    }else{
+      Y = oligo.mat[ix,]
+    }
     Y <- melt(Y)
 
     Y$X <- X
@@ -229,258 +499,4 @@ smoother <- function(y, x=NULL, weights=NULL, chr=chr, maxGapSmooth=maxGapSmooth
   ret.all$idx <- NULL
 
   return(ret.all)
-}
-
-#' A function for the WGBS version of the 'BumphunterEngine' function with 'LM'
-#' and 'GLM'
-#'
-#' This function is the WGBS version of the 'BumphunterEngine' function with 'LM'
-#' and 'GLM'
-#'
-#' @param oligo.mat
-#' @param design
-#' @param chr Defaults to NULL
-#' @param pos
-#' @param cluster Defaults to NULL
-#' @param coef
-#' @param minInSpan Defaults to 10
-#' @param minNum Defaults to 10
-#' @param minNumRegion Defaults to 5
-#' @param cutoff Defaults to NULL
-#' @param maxGap Defaults to 50
-#' @param maxGapSmooth Defaults to 1e8
-#' @param smooth Defaults to FALSE
-#' @param bpSpan Defaults to 100
-#' @param verbose Defaults to TRUE
-#' @param workers Defaults to NULL
-#' @import
-#' @keywords inference
-#' @return
-
-##The WGBS version of the 'BumphunterEngine' function with 'LM' and 'GLM'
-bumphunt = function(oligo.mat, design, 
-                            chr = NULL, pos, coef = 2, minInSpan=10, minNum=10, 
-                            minNumRegion=5,
-                            cutoff = NULL, maxGap = 50, maxGapSmooth=50,
-                            smooth = FALSE, bpSpan=100,
-                            verbose = TRUE, workers=NULL, ...)
-{
-  if (!is.matrix(oligo.mat))
-    stop("'oligo.mat' must be a matrices.")
-  if (ncol(oligo.mat) != nrow(design))
-    stop("Total number of columns in 'oligo.mat' must  match number of rows of 'design'")
-  if (!(is.null(cutoff) || length(cutoff) %in% 1:2))
-    stop("'cutoff' has to be either NULL or a vector of length 1 or 2")
-  if (length(cutoff) == 2)
-    cutoff <- sort(cutoff)
-
-  if (!getDoParRegistered())
-    registerDoSEQ()
-  registerDoParallel()
-  if (is.null(workers)) { workers <- 1 }
-  backend <- getDoParName()
-  version <- getDoParVersion()
-  subverbose <- max(as.integer(verbose) - 1L, 0)
-  if (verbose) {
-    if (workers == 1) {
-      mes <- "Using a single core (backend: %s, version: %s)."
-      message(sprintf(mes, backend, version))
-    }else {
-      mes <- "Parallelizing using %s workers/cores (backend: %s, version: %s)."
-      message(sprintf(mes, workers, backend, version))
-    }
-  }
-  if (is.null(chr))
-    chr <- rep("Unspecified", length(pos))
-  if (is.factor(chr))
-    chr <- as.character(chr)
-  
-  cluster <- bumphunter:::clusterMaker(chr, pos, maxGap = maxGap)
-  
-  if (verbose)
-    message("Computing coefficients.")
-
-  est <- bumphunter:::.getEstimate(log2(oligo.mat+1), design, coef, full=TRUE) 
-  rawBeta <- est$coef
-  
-  if (smooth) {
-    sd.raw = est$sigma*est$stdev.unscaled
-    weights <- sd.raw 
-    weights[sd.raw < 10^-5] = mean(sd.raw[sd.raw > 10^-5]) 
-    rm(sd.raw)
-  }
-  rm(est)
-  gc()
-
-  if (smooth) {
-    if (verbose)
-      message("Smoothing coefficients.")
-
-    beta <- vector("list", 3)
-    beta[[1]] <- beta[[2]] <- rep(NA, length(pos))
-    for (chromosome in unique(chr)){
-      beta.tmp <- smoother(y = rawBeta[chr==chromosome],
-                           x = pos[chr==chromosome],
-                           workers=workers, chr=chr[chr==chromosome],
-                           maxGapSmooth=maxGapSmooth, weights = weights,
-                           minNum = minNum, minInSpan = minInSpan, bpSpan = bpSpan,
-                           verbose = verbose)
-      beta[[1]][chr==chromosome] <- beta.tmp[[1]]
-      beta[[2]][chr==chromosome] <- beta.tmp[[2]]
-    }
-    beta[[3]] <- beta.tmp[[3]]
-    names(beta) <- names(beta.tmp)
-    rm(beta.tmp)
-    Index <- which(beta$smoothed)
-    beta <- beta$fitted
-
-  }else {
-    beta <- rawBeta
-    Index <- seq(along = beta)
-  }
-  rm(rawBeta)
-  gc()
-  if (verbose)
-    message("Finding regions.")
-  tab <- regionFinder(x = beta, chr = chr, pos = pos, cluster = cluster,
-                      cutoff = cutoff, ind = Index, minNumRegion = minNumRegion,
-                      oligo.mat = oligo.mat,
-                      verbose = TRUE,
-                      design = design, workers=workers)
-  rm(beta);
-  rm(chr);
-  rm(pos);
-  gc()
-  if (nrow(tab) == 0) {
-    if (verbose)
-      message("No candidate regions found!")
-    return(NA)
-  }else {
-    if (verbose)
-      message(sprintf("Found %s candidate regions.", nrow(tab)))
-  }
-
-  return(table = tab)
-}
-
-#' A function for the WGBS version of the 'BumphunterEngine' function with 'LM'
-#' and 'GLM'
-#'
-#' This function is the WGBS version of the 'BumphunterEngine' function with 'LM'
-#' and 'GLM'
-#'
-#' @param OligoSignal
-#' @param conditionLabels
-#' @param design
-#' @param coef
-#' @param minInSpan Defaults to 10
-#' @param minNum Defaults to 70
-#' @param minNumRegion Defaults to 5
-#' @param cutoff Defaults to NULL
-#' @param nullMethod Defaults to GLMM
-#' @param smooth Defaults to FALSE
-#' @param bpSpan Defaults to 1000
-#' @param verbose Defaults to TRUE
-#' @param workers Defaults to NULL
-#' @param sampleSize Defaults to (ncol(OligoSignal)-1)/2
-#' @param maxPerms 
-#' @import
-#' @keywords inference
-#' @return
-#' @export
-
-DRfinder <- function(OligoSignal, 
-                     conditionLabels=c("condition1", "condition2"),
-                     minInSpan=10,
-                      design,
-                      coef = 2, minNum=70, bpSpan=1000, 
-                      minNumRegion=5, 
-                      cutoff = NULL, 
-                      smooth = FALSE,
-                      verbose = TRUE,
-                      workers = NULL, 
-                     sampleSize=(ncol(OligoSignal)-1)/2,
-                      maxPerms=50){
-
-  cond <- c(rep(conditionLabels[1], sampleSize), 
-            rep(conditionLabels[2], sampleSize))
-  design = model.matrix(~cond)
-  
-  nlocs = nrow(OligoSignal) 
-  oligo.mat = OligoSignal[,-1]
-  
-  chr = rownames(OligoSignal)
-  pos = OligoSignal[,1]
-  rm(OligoSignal)
-  
-  maxGap <- maxGapSmooth <-max(diff(pos))+1
-  
-  # get observed stats
-
-  res = bumphunt(oligo.mat, 
-                        minInSpan=minInSpan,
-                        design = design, chr = chr, pos = pos, 
-                        coef = coef, minNum=minNum, maxGapSmooth=maxGapSmooth, 
-                        minNumRegion=minNumRegion,
-                        cutoff = cutoff, maxGap = maxGap, 
-                        smooth = smooth, 
-                        verbose = verbose,
-                        workers = workers) 
-
-
-  if (nrow(design)%%2==0){
-    perms <- combn(seq(1, nrow(design)), sampleSize)
-    perms <- perms[, 2:(ncol(perms)/2)]
-    res.flip <- NULL
-    
-    if (maxPerms < ncol(perms)){
-      # subset on 'balanced perms'
-      if (sampleSize > 3 & sampleSize < 6){ 
-        sg <- apply(perms, 2, function(x) sum(x > sampleSize))
-        perms <- perms[, sg < (sampleSize-1) & sg >= 2 ]
-        maxPerms <- min(maxPerms, ncol(perms))
-      }else if(sampleSize >= 6){
-        sg <- apply(perms, 2, function(x) sum(x > sampleSize))
-        perms <- perms[, sg >= floor(sampleSize/2) & sg <= ceiling(sampleSize/2) ]
-      }
-      perms <- perms[,sort(sample(1:ncol(perms), maxPerms, replace=FALSE))]
-    }
-    
-    # Now rerun on flipped designs and concatenate results
-    for (j in 1:ncol(perms)){
-      reorder <- perms[,j]
-      designr <- design
-      designr[,2] <- 0
-      designr[reorder,2] <- 1
-      
-      res.flip.p = bumphunt(oligo.mat, 
-                                   minInSpan=minInSpan,
-                                   design = designr, chr = chr, pos = pos, 
-                                   coef = coef, minNum=minNum, 
-                                   maxGapSmooth=maxGapSmooth, 
-                                   minNumRegion=minNumRegion,
-                                   cutoff = cutoff,  maxGap = maxGap, 
-                                   smooth = smooth,
-                                   verbose = verbose,
-                                   workers = workers) 
-      res.flip.p$permNum <- j 
-      
-      res.flip <- rbind(res.flip, res.flip.p)
-      print(paste0(j, " out of ", ncol(perms), " permutations completed"))
-    }	
-  }else{
-    stop("Error: Currently only balanced designs supported")
-  }
-  rm(res.flip.p)
-  
-  perm.ordered <- c(sort(abs(res$stat), method="quick"), Inf)
-  pval <- rep(NA, nrow(res))
-  pval[!is.na(res$stat)] <- (1 + 
-                          sapply(abs(res$stat[!is.na(res$stat)]), 
-                                function(x) length(perm.ordered) - 
-                                min(which(x <= perm.ordered)))) /
-                           (1 + sum(!is.na(res$stat)))							
-  res$pval <- pval
-  res$qval <- p.adjust(pval, method="BH")
-  return(res)
 }
